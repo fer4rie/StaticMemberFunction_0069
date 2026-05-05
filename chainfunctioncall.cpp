697606abedb090bba58b09d33c69b649bbf0a105
fer4rie <firapuspita2106@gmail.com>
2026-05-05 18:13:54 +0700

    chain function

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -5,3 +5,13 @@ class buku {
     string judul;
 
     public:
+
+    buku &setJudul(string judul) {
+        this->judul = judul;
+        return *this; // chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+};
