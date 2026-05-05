67d96d3773bff5c9e4f47c814077650be19fb260
fer4rie <firapuspita2106@gmail.com>
2026-05-05 17:37:09 +0700

    pembuatan static function

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -11,3 +11,19 @@ public:
 
     void setID();
     void printAll();
+
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) : nama(pnama) {
+        setID();
+    };
+};
+
+int Mahasiswa::nim = 0;
